d71c334daaffce07ad05d2f5927b062cd85ea57a
jonschlinkert <jon.schlinkert@sellside.com>
2015-07-09 19:39:12 -0400

    refactor, simplify

--- test.js
+++ test.js
@@ -1,60 +1,49 @@
-/*!
- * parse-filepath <https://github.com/jonschlinkert/parse-filepath>
- *
- * Copyright (c) 2014, Jon Schlinkert, Brian Woodward, contributors.
- * Licensed under the MIT License
- */
-
 'use strict';
 
+/* deps: mocha */
 var path = require('path');
 var should = require('should');
 var parsePath = require('./');
-
-function normalize(str) {
-  return path.normalize(str);
-}
-
 
 describe('parse-filepath:', function() {
   describe('dotfiles', function() {
     it('should recognize dotfiles', function() {
       parsePath('foo/bar/baz/.dotfile').should.eql({
-        name: 'dotfile',
-        dirname: normalize('foo/bar/baz'),
+        name: '.dotfile',
+        dirname: 'foo/bar/baz',
         extname: '',
         basename: '.dotfile',
-        extSegments: []
+        root: '',
       });
     });
 
     it('should recognize .gitignore', function() {
       parsePath('./.gitignore').should.eql({
-        name: 'gitignore',
+        name: '.gitignore',
         dirname: '.',
         extname: '',
         basename: '.gitignore',
-        extSegments: []
+        root: '',
       });
     });
 
     it('should recognize config files', function() {
       parsePath('./.verbfile.md').should.eql({
-        name: 'verbfile',
+        name: '.verbfile',
         dirname: '.',
         extname: '.md',
         basename: '.verbfile.md',
-        extSegments: ['.md']
+        root: '',
       });
     });
 
     it('should recognize config files', function() {
       parsePath('./.travis.yml').should.eql({
-        name: 'travis',
+        name: '.travis',
         dirname: '.',
         extname: '.yml',
         basename: '.travis.yml',
-        extSegments: ['.yml']
+        root: '',
       });
     });
   });
@@ -63,10 +52,10 @@ describe('parse-filepath:', function() {
     it('should return the correct values', function() {
       parsePath('foo').should.eql({
         name: 'foo',
-        dirname: '.',
+        dirname: '',
         extname: '',
         basename: 'foo',
-        extSegments: []
+        root: '',
       });
     });
 
@@ -76,7 +65,7 @@ describe('parse-filepath:', function() {
         dirname: '.',
         extname: '',
         basename: 'foo',
-        extSegments: []
+        root: '',
       });
     });
   });
@@ -85,10 +74,20 @@ describe('parse-filepath:', function() {
     it('should return an object of path parts', function() {
       parsePath('foo/bar/baz/index.html').should.eql({
         name: 'index',
-        dirname: normalize('foo/bar/baz'),
+        dirname: 'foo/bar/baz',
         extname: '.html',
         basename: 'index.html',
-        extSegments: ['.html']
+        root: '',
+      });
+    });
+
+    it('should get the root', function() {
+      parsePath('/foo/bar/baz/index.html').should.eql({
+        name: 'index',
+        dirname: '/foo/bar/baz',
+        extname: '.html',
+        basename: 'index.html',
+        root: '/',
       });
     });
   });
@@ -96,11 +95,11 @@ describe('parse-filepath:', function() {
   describe('when a filepath ends with a slash', function() {
     it('dirname should be the full filepath, and basename should be empty', function() {
       parsePath('foo/bar/baz/quux/').should.eql({
-        name: '',
-        dirname: normalize('foo/bar/baz/quux/'),
+        name: 'quux',
+        dirname: 'foo/bar/baz',
         extname: '',
-        basename: '',
-        extSegments: []
+        basename: 'quux',
+        root: '',
       });
     });
   });
@@ -108,11 +107,11 @@ describe('parse-filepath:', function() {
   describe('when a filepath with multiple extensions is passed', function() {
     it('should return an object of path parts', function() {
       parsePath('foo/bar/baz/index.md.html').should.eql({
-        name: 'index',
-        dirname: normalize('foo/bar/baz'),
-        extname: '.md.html',
+        name: 'index.md',
+        dirname: 'foo/bar/baz',
+        extname: '.html',
         basename: 'index.md.html',
-        extSegments: ['.md', '.html']
+        root: '',
       });
     });
   });
@@ -121,10 +120,10 @@ describe('parse-filepath:', function() {
     it('should return an object of path parts', function() {
       parsePath('foo/bar/baz/index').should.eql({
         name: 'index',
-        dirname: normalize('foo/bar/baz'),
+        dirname: 'foo/bar/baz',
         extname: '',
         basename: 'index',
-        extSegments: []
+        root: '',
       });
     });
   });
@@ -132,56 +131,31 @@ describe('parse-filepath:', function() {
   describe('when a dirname is "."', function() {
     it('should preserve the basename', function() {
       parsePath('index.js').should.eql({
-        dirname: '.',
+        dirname: '',
         basename: 'index.js',
         name: 'index',
         extname: '.js',
-        extSegments: ['.js'],
+        root: ''
       });
     });
-  });
-
-  describe('when a filepath with zero extensions is passed', function() {
-    it('should return an object of path parts', function() {
+    it('should parse paths without extensions', function() {
       parsePath('foo/bar/baz/index').should.eql({
         name: 'index',
-        dirname: normalize('foo/bar/baz'),
+        dirname: 'foo/bar/baz',
         extname: '',
         basename: 'index',
-        extSegments: []
+        root: '',
       });
     });
-  });
-});
-
-describe('utils.ext()', function() {
-  describe('when a filepath with multiple extensions is passed:', function() {
-    it('should return the extension from options.ext', function() {
-      parsePath('foo/bar/baz.min.js').extname.should.eql('.min.js');
-    });
 
     it('should return the extension from options.ext', function() {
-      parsePath('foo/bar/baz.min.js').extSegments.should.eql(['.min', '.js']);
+      parsePath('foo/bar/baz.min.js').extname.should.eql('.js');
     });
-  });
-
-  describe('when a filepath with a single extension is passed:', function() {
-    it('should return the extname', function() {
+    it('should parse the extname', function() {
       parsePath('foo/bar/baz.js').extname.should.eql('.js');
     });
-
-    it('should return an array with the extname', function() {
-      parsePath('foo/bar/baz.js').extSegments.should.eql(['.js']);
-    });
-  });
-
-  describe('when a filepath with no extensions is passed:', function() {
-    it('should return an empty extname', function() {
+    it('should return an empty extname when no extension is passed', function() {
       parsePath('foo/bar/baz').extname.should.eql('');
-    });
-
-    it('should return an empty extSegments array', function() {
-      parsePath('foo/bar/baz').extSegments.should.eql([]);
     });
   });
 });
