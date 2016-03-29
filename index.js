f26fb345c6858c784e043913eaa240b0690fd4ba
jonschlinkert <jon.schlinkert@sellside.com>
2016-03-29 01:06:24 -0400

    make calculated paths getters

--- index.js
+++ index.js
@@ -3,46 +3,85 @@
 var path = require('path');
 var isAbsolute = require('is-absolute');
 var MapCache = require('map-cache');
-var nativeParse = path.parse;
 var cache = new MapCache();
 
-module.exports = function parse(fp) {
-  if (typeof fp !== 'string') {
-    throw new Error('parse-filepath expects a string.');
+module.exports = function(filepath) {
+  if (typeof filepath !== 'string') {
+    throw new TypeError('parse-filepath expects a string');
   }
 
-  if (cache.has(fp)) {
-    return cache.get(fp);
+  if (cache.has(filepath)) {
+    return cache.get(filepath);
   }
 
-  var res = { path: fp, isAbsolute: isAbsolute(fp) };
-  var params = {
-    base: 'basename',
-    dir: 'dirname',
-    ext: 'extname',
-    name: 'name',
-    root: 'root'
-  };
-
-  res.absolute = path.resolve(res.path);
-
-  if (typeof nativeParse === 'function') {
-    var parsed = nativeParse(fp);
-    for (var key in parsed) {
-      if (parsed.hasOwnProperty(key)) {
-        res[params[key]] = parsed[key];
-      }
-    }
+  var obj = {};
+  if (typeof path.parse === 'function') {
+    obj = path.parse(filepath);
+    obj.extname = obj.ext;
+    obj.basename = obj.base;
+    obj.dirname = obj.dir;
+    obj.stem = obj.name;
 
   } else {
-    var ext = path.extname(fp);
-    res.name = path.basename(fp, ext);
-    res.basename = res.name + ext;
-    res.extname = path.extname(fp);
-    res.dirname = path.dirname(fp);
-    res.root = '';
+    obj.root = '';
+
+    define(obj, 'extname', function() {
+      return path.extname(filepath);
+    });
+
+    define(obj, 'ext', function() {
+      return this.extname;
+    });
+
+    define(obj, 'name', function() {
+      return path.basename(filepath, this.ext);
+    });
+
+    define(obj, 'stem', function() {
+      return this.name;
+    });
+
+    define(obj, 'base', function() {
+      return this.name + this.ext;
+    });
+
+    define(obj, 'basename', function() {
+      return this.base;
+    });
+
+    define(obj, 'dir', function() {
+      return path.dirname(filepath);
+    });
+
+    define(obj, 'dirname', function() {
+      return this.dir;
+    });
   }
 
-  cache.set(fp, res);
-  return res;
+  obj.path = filepath;
+
+  define(obj, 'absolute', function() {
+    return path.resolve(this.path);
+  });
+
+  define(obj, 'isAbsolute', function() {
+    return isAbsolute(this.path);
+  });
+
+  cache.set(filepath, obj);
+  return obj;
 };
+
+function define(obj, prop, fn) {
+  var cached;
+  Object.defineProperty(obj, prop, {
+    configurable: true,
+    enumerable: true,
+    set: function(val) {
+      cached = val;
+    },
+    get: function() {
+      return cached || (cached = fn.call(obj));
+    }
+  });
+}
